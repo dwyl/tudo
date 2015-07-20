03da84f88b7e3628410dbdcb587aa4a6e28aba78
Nelson <contact.nelsonic@gmail.com>
2015-07-20 22:51:44 +0100

    decent looking login page. just need to store the auth token in cookie/redis to crack on. https://github.com/dwyl/tudo/issues/6

--- views/index.js
+++ views/index.js
@@ -1,3 +1,8 @@
+var fs = require('fs');
+
 module.exports = {
-  hello : require(__dirname +'/hello.tag')
+  hello  : require(__dirname +'/hello.tag'),
+  header : fs.readFileSync(__dirname +'/layout_header.html', 'utf8'),
+  footer : fs.readFileSync(__dirname +'/layout_footer.html', 'utf8'),
+  login  : require(__dirname +'/login.tag')
 }
