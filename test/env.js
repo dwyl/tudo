8665d4525cf5fed08a4356d89aa7e31c3b03b900
Nelson <contact.nelsonic@gmail.com>
2015-07-21 11:30:32 +0100

    removes console.logs from test/env.js https://github.com/dwyl/tudo/issues/18

--- test/env.js
+++ test/env.js
@@ -11,25 +11,19 @@ test("TEMPORARILY RENAME env.json file to force the try/catch error in lib/env.j
   decache('../lib/env');
   try {
     if(require(envfile)) { // check if the file exists!
-      console.log(' - - > ' + envfile +' EXISTS!');
       var env = require(envfile);
-      console.log(env);
-      fs.renameSync(envfile, tempenv);
-      decache(envfile);
-      // delete all environment variables set by env.js
       var keys = Object.keys(env)
       keys.map(function(k){
         delete process.env[k];
-      })
+      });
+      fs.renameSync(envfile, tempenv);
+      decache(envfile);
     }
   }
   catch (e) {
     console.log(envfile + ' NOT exist!')
     // do nothing!. if it failed that's fine!
   }
-  // fs.renameSync(envfile, tempenv);
-  // now require the ./lib/env.js and expect it to fail
-  // fs.renameSync(envfile, tempenv);
   decache('../lib/env');
   require('../lib/env'); // this should spit out an error message now!
   // console.log(" - - - - - > " +process.env.GITHUB_CLIENT_ID);
@@ -46,7 +40,7 @@ test("CREATE the env.json file from env.json_sample if it does not exist", funct
     var envfile = path.resolve(__dirname + '/../env.json');
     try { // check the tempenv file was created from the previous test
 
-      console.log(fs.lstatSync(tempenv).birthtime);
+      // console.log(fs.lstatSync(tempenv).birthtime);
       fs.renameSync(tempenv, envfile); // restore the env.json from tempenv.json
     }
     catch (e) { // else create it from the _sample file!
@@ -58,13 +52,3 @@ test("CREATE the env.json file from env.json_sample if it does not exist", funct
     t.end();
   },100);
 });
-
-//
-// var server = require('../server'); // test server which in turn loads our module
-//
-// test("Check the GITHUB_CLIENT_ID environenment variable is set", function(t) {
-//   decache('../lib/env')
-//   require('../lib/env'); // initialise environment variables
-//   t.ok(process.env.GITHUB_CLIENT_ID, "GITHUB_CLIENT_ID environment variable is set!")
-//   t.end();
-// });
