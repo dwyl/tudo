b4ee4af2dca1fd3a95af063e2f8a8e6b6e37ceec
Nelson <contact.nelsonic@gmail.com>
2015-07-21 11:28:08 +0100

    adds badges to readme https://github.com/dwyl/tudo/issues/23

--- server.js
+++ server.js
@@ -17,29 +17,15 @@ function hello_handler (request, reply) {
 }
 
 function auth_handler (request, reply) {
-	if(request.auth.credentials) {
-		console.log(' - - - - - - - - - - - - - - - - - - - -')
-		console.log(request.auth.credentials)
-		console.log(' - - - - - - - - - - - - - - - - - - - -')
-	}
+	// if the credentials are set don't force people to re-login!
+	// if(request.auth.credentials) {
+	// 	console.log(' - - - - - - - - - - - - - - - - - - - -')
+	// 	console.log(request.auth.credentials)
+	// 	console.log(' - - - - - - - - - - - - - - - - - - - -')
+	// }
 	var body = riot.render(views.login, {GITHUB_CLIENT_ID : process.env.GITHUB_CLIENT_ID })
 	reply(views.header + body + views.footer);
 }
-//
-// server.register(require('bell'), function(err){
-//     server.auth.strategy('github', 'bell', {
-//         provider: 'github',
-//         password: 'password',
-//         clientId: process.env.GITHUB_CLIENT_ID,
-//         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-//         isSecure: false, // please confirm which of these permissions we need ...
-//         // scope: ['write', 'read:org', 'user', 'admin:org', 'write:org', 'repo', 'read:repo_hook', 'write:repo_hook'],
-// 				scope: ['repo'],
-//         providerParams: {
-//             redirect_uri: server.info.uri + '/login'
-//         }
-//     });
-// });
 
 server.route([
 	{ method: 'GET', path: '/login',          handler: auth_handler },
