061b62abc134a04a44d1258ec1a0bd1ec458a6c2
Jack Rans <jwrans@gmail.com>
2015-07-23 11:10:37 +0100

    restructured handler to only give whast necessary for the first sprint, i.e. adding all users issues and a user and getting them back

--- api/db_handlers.js
+++ api/db_handlers.js
@@ -1,47 +1,66 @@
-function addIssue (client, obj, callback) {
-    var timeMS = Date.parse(obj.created_at);
-    var multi  = client.multi();
 
-    multi.hmset("issue:" + obj.id, obj);
-    multi.zadd("issues", timeMS, obj.id);
-    multi.exec(function (errors, replies) {
-        return callback(errors, replies);
-    });
-}
-
-function deleteIssueById (client, issueId, callback) {
-    var multi = client.multi();
-
-    multi.zrem("issues", issueId);
-    multi.del("issue:" + issueId);
-    multi.exec(function (errors, replies) {
-        return callback(errors, replies);
-    });
-}
 
 function addUser (client, obj, callback) {
     var multi  = client.multi();
 
-    multi.hmset("user:" + obj.id, obj);
-    multi.sadd("users", obj.id);
+    multi.hmset("user:" + obj.username, obj);
+    multi.sadd("users", obj.username);
     multi.exec(function (errors, replies) {
         return callback(errors, replies);
     });
 }
 
-function deleteUserById (client, userId, callback) {
+function getUserByUsername (client, username, callback) {
+    client.hgetall("user:" + username, callback);
+}
+
+
+function addIssue (client, obj, callback) {
+    client.hmset("issue:" + obj.id, obj, callback);
+}
+
+function addIssueToUserList (client, username, issueId, lastUpdateTime, callback) {
+    var timeMS = Date.parse(lastUpdateTime);
+
+    client.zadd(username + ":issues", timeMS, issueId, callback)
+}
+
+function addIssuesByUsername (client, username, issuesArray, callback) {
+    var multi  = client.multi();
+
+    issuesArray.forEach(function (issue) {
+        addIssue(multi, issue);
+        addIssueToUserList(multi, username, issue.id, issue.updated_at);
+    })
+    multi.exec(function (errors, replies) {
+        return callback(errors, replies);
+    });
+}
+
+function getIssueListByUsername (client, username, callback) {
+    client.zrange(username + ":issues",0,-1, callback); //err, data
+}
+
+function getIssuesByList (client, issueList, callback) {
     var multi = client.multi();
-
-    multi.srem("users", userId);
-    multi.del("user:" + userId);
-    multi.exec(function (errors, replies) {
-        return callback(errors, replies);
+    issueList.forEach(function (issueId) {
+      multi.hgetall("issue:" + issueId)
+    });
+    multi.exec(function (err, replies) {
+      return callback(err, replies);
     });
 }
+
+function getIssuesByUsername (client, username, callback) {
+    getIssueListByUsername(client, username, function(err, data) {
+        getIssuesByList (client, data, callback);
+    })
+}
+
 
 module.exports = {
-    addIssue: addIssue,
-    deleteIssueById: deleteIssueById,
     addUser: addUser,
-    deleteUserById: deleteUserById
+    getUserByUsername: getUserByUsername,
+    addIssuesByUsername: addIssuesByUsername,
+    getIssueListByUsername: getIssueListByUsername
 };
--- test/db_handlers.test.js
+++ test/db_handlers.test.js
@@ -6,35 +6,35 @@ var DBHandlers  = require("../api/db_handlers.js");
 var connection  = url.parse(process.env.TEST_REDISCLOUD_URL);
 var redisClient = redisConfig(connection);
 
-test("Adding an issue to DB", function (t) {
-    var testIssue = {
-        id: 12345678,
-        created_at: "2015-06-22T09:22:51Z",
-    };
+// test("Adding an issue to DB", function (t) {
+//     var testIssue = {
+//         id: 12345678,
+//         updated_at: "2015-06-22T09:22:51Z",
+//     };
+//
+//     DBHandlers.addIssue(redisClient, testIssue, function (errors, replies) {
+//         t.equal(errors, null, "add errors null");
+//         t.deepEqual(replies, ["OK",1], "should get an OK from setting hash, and 1 for addition to set of issues");
+//         DBHandlers.deleteIssueById(redisClient, testIssue.id, function (errors, replies) {
+//             t.end();
+//         });
+//     });
+// });
 
-    DBHandlers.addIssue(redisClient, testIssue, function (errors, replies) {
-        t.equal(errors, null, "add errors null");
-        t.deepEqual(replies, ["OK",1], "should get an OK from setting hash, and 1 for addition to set of issues");
-        DBHandlers.deleteIssueById(redisClient, testIssue.id, function (errors, replies) {
-            t.end();
-        });
-    });
-});
-
-test("Deleting an issue by id", function (t) {
-    var testIssue = {
-        id: 23456789,
-        created_at: "2015-06-22T09:22:51Z",
-    };
-
-    DBHandlers.addIssue(redisClient, testIssue, function (errors, replies) {
-        DBHandlers.deleteIssueById(redisClient, testIssue.id, function (errors, replies) {
-            t.equal(errors, null, "delete errors null");
-            t.deepEqual(replies, [1,1], "1 deleted from set of issues and database respectively");
-            t.end();
-        });
-    });
-});
+// test("Deleting an issue by id", function (t) {
+//     var testIssue = {
+//         id: 23456789,
+//         created_at: "2015-06-22T09:22:51Z",
+//     };
+//
+//     DBHandlers.addIssue(redisClient, testIssue, function (errors, replies) {
+//         DBHandlers.deleteIssueById(redisClient, testIssue.id, function (errors, replies) {
+//             t.equal(errors, null, "delete errors null");
+//             t.deepEqual(replies, [1,1], "1 deleted from set of issues and database respectively");
+//             t.end();
+//         });
+//     });
+// });
 
 test("Adding a user to DB", function (t) {
     var testUser = {
@@ -44,21 +44,18 @@ test("Adding a user to DB", function (t) {
     DBHandlers.addUser(redisClient, testUser, function (errors, replies) {
         t.equal(errors, null, "add errors null");
         t.deepEqual(replies, ["OK",1], "should get an OK from setting hash, and 1 for addition to set of users");
-        DBHandlers.deleteUserByUsername(redisClient, testUser.username, function (errors, replies) {
-            t.end();
-        });
+        t.end();
     });
 });
 
-test("Deleting a user by id", function (t) {
+test("Get user by username", function (t) {
     var testUser = {
-        id: 23456789,
+        userName: 'testUser'
     };
-
     DBHandlers.addUser(redisClient, testUser, function (errors, replies) {
-        DBHandlers.deleteUserByUsername(redisClient, testUser.username, function (errors, replies) {
-            t.equal(errors, null, "delete errors null");
-            t.deepEqual(replies, [1,1], "1 deleted from set of users and database respectively");
+        DBHandlers.getUserByUsername(redisClient, testUser, function(errors, replies) {
+            t.deepEqual(replies, testUser);
+            DBHandlers.deleteUserByUsername(redisClient, testUser.id);
             t.end();
         });
     });
@@ -79,27 +76,17 @@ test("When an issue is created it's assigned to a user", function (t) {
 
     DBHandlers.addUser(redisClient, testUser, function (errors, replies) {
         DBHandlers.addIssue(redisClient, testIssue, function (errors, replies) {
-            DBHandlers.getIssueByID(redisClient, testIssue.id, function (errors, replies) {
+            DBHandlers.getIssuesByList(redisClient, function (errors, replies) {
                 t.deepEqual(replies, redisClient.zrange(testUser.userName + ":issues", 0, -1));
                 t.end();
-                DBHandlers.deleteIssueById(redisClient, testIssue.id, function (errors, replies) {});
-                DBHandlers.deleteUserById(redisClient, testUser.id, function (errors, replies) {});
+                DBHandlers.deleteIssueById(redisClient, testIssue.id);
+                DBHandlers.deleteUserByUsername(redisClient, testUser.id);
             });
         });
     });
 });
 
-test("Get user by username", function (t) {
-    var testUser = {
-        id: 88889999,
-        userName: 'testUser'
-    };
-    DBHandlers.addUser(redisClient, testUser, function (errors, replies) {
-        DBHandlers.getUserByUsername(redisClient, testUser, function(errors, replies) {
-            t.deepEqual(replies, testUser);
-        });
-    });
-});
+
 
 test("Close clients after tests completed", function (t) {
     redisClient.end()
