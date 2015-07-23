74809a1d9c3b3c50c7c4ceaab8e38189b277bb98
Danielwhyte <danwhy@gmail.com>
2015-07-23 10:18:01 +0100

    updated tests and wrote new tests for database functions

--- test/db_handlers.test.js
+++ test/db_handlers.test.js
@@ -44,7 +44,7 @@ test("Adding a user to DB", function (t) {
     DBHandlers.addUser(redisClient, testUser, function (errors, replies) {
         t.equal(errors, null, "add errors null");
         t.deepEqual(replies, ["OK",1], "should get an OK from setting hash, and 1 for addition to set of users");
-        DBHandlers.deleteUserById(redisClient, testUser.id, function (errors, replies) {
+        DBHandlers.deleteUserByUsername(redisClient, testUser.username, function (errors, replies) {
             t.end();
         });
     });
@@ -56,10 +56,47 @@ test("Deleting a user by id", function (t) {
     };
 
     DBHandlers.addUser(redisClient, testUser, function (errors, replies) {
-        DBHandlers.deleteUserById(redisClient, testUser.id, function (errors, replies) {
+        DBHandlers.deleteUserByUsername(redisClient, testUser.username, function (errors, replies) {
             t.equal(errors, null, "delete errors null");
             t.deepEqual(replies, [1,1], "1 deleted from set of users and database respectively");
             t.end();
+        });
+    });
+});
+
+test("When an issue is created it's assigned to a user", function (t) {
+    var testUser = {
+        id: 88889999,
+        userName: 'testUser'
+    };
+    var testIssue = {
+        id: 23456789,
+        created_at: "2015-06-22T09:22:51Z",
+        assignee: {
+            id: 88889999
+        }
+    };
+
+    DBHandlers.addUser(redisClient, testUser, function (errors, replies) {
+        DBHandlers.addIssue(redisClient, testIssue, function (errors, replies) {
+            DBHandlers.getIssueByID(redisClient, testIssue.id, function (errors, replies) {
+                t.deepEqual(replies, redisClient.zrange(testUser.userName + ":issues", 0, -1));
+                t.end();
+                DBHandlers.deleteIssueById(redisClient, testIssue.id, function (errors, replies) {});
+                DBHandlers.deleteUserById(redisClient, testUser.id, function (errors, replies) {});
+            });
+        });
+    });
+});
+
+test("Get user by username", function (t) {
+    var testUser = {
+        id: 88889999,
+        userName: 'testUser'
+    };
+    DBHandlers.addUser(redisClient, testUser, function (errors, replies) {
+        DBHandlers.getUserByUsername(redisClient, testUser, function(errors, replies) {
+            t.deepEqual(replies, testUser);
         });
     });
 });
