ba774617cf4cf711e1126db0a6a028be43f36f8d
iteles <code@inesteles.com>
2015-08-19 16:20:36 +0100

    [dwyl/team-beach] loops through hardcoded data in issues.tag #119

--- test/issues_page.test.js
+++ test/issues_page.test.js
@@ -11,3 +11,27 @@ test('Visit issues page', function(t){
     t.end();
   });
 });
+
+test('The 5 priority levels appear on the page', function(t){
+  server.inject({url:'/issues', method:'GET'}, function(res){
+    
+    var priority_nums = [1,2,3,4,5];
+    
+    //passes each element in array as the 'level' into the callback
+    //every returns true or false
+    var all_present = priority_nums.every(function(level){
+      return res.payload.indexOf('p' + level) > -1;
+    })
+    
+    t.ok(all_present, 'All different priority levels are present');
+  
+    // t.ok(res.payload.indexOf('Priority') > -1, 'Issues page has the word "Priority"');
+    server.stop(function(){});
+    t.end();
+  });
+});
+
+
+
+
+
--- views/tags/home/issues.tag
+++ views/tags/home/issues.tag
@@ -1,10 +1,17 @@
 <issues>
 
-  <div class='priority-level p1'>
-    <h4>Priority 1</h4>
+  <div each={i in priority_levels} class={'priority-level p' + i}>
+    <h4>Priority {i}</h4>
     <ul class='issues-list'>
     	<issue />
     </ul>
   </div>
+  
+  <script>
+  this.priority_levels = [
+    1,2,3,4,5
+  ]
+  </script>
+  
 
 </issues>
