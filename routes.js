9ef3c81108e4afd19054c4e9a481afb014141dd4
iteles <code@inesteles.com>
2015-08-19 17:16:16 +0100

    Fixes route for "/" to render with new prefix and suffix files, fixes https://github.com/dwyl/tudo/issues/118

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ var routes = [
       method: 'GET',
       path: '/',
       handler: function(req, reply){
-        reply(tags.header + riot.render(tags.login, {GITHUB_CLIENT_ID:process.env.GITHUB_CLIENT_ID}) + tags.footer)
+        reply(tags.prefix + riot.render(tags.login, {GITHUB_CLIENT_ID:process.env.GITHUB_CLIENT_ID}) + tags.suffix)
       }
   },
   {
