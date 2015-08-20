db3777610d37c6f1d40e7251e884b2450df26b5b
Nikhila Ravi <nikkiravi92@gmail.com>
2015-08-20 18:31:07 +0100

    [dwyl/team-beach] adds parser to format issues fixture fixes #129

--- handlers/issues_handler.js
+++ handlers/issues_handler.js
@@ -6,8 +6,44 @@ exports.handler = function (req, reply) {
 }
 
 exports.parse = function (entries) {
-  if (typeof entries === 'string') {
-    entries = JSON.parse(entries);
+
+  //extract priorities from issue
+  function find_priorities(issue) {
+    return issue.labels.filter(function(label) {
+      return label.match(/priority-\d/);
+    });
   }
-  return [{issues: [], priority: NaN}, {issues: [], priority: NaN}, {issues: [], priority: NaN}]
+
+  //if multiple priority labels are present choose first one or if no priority labels assign to unprioritised
+  function decide_priority(priority_array) {
+    return priority_array.reduce(function(priority, priority_string) {
+      return priority === "unprioritised" ? priority_string.split("-")[1] : priority;
+    }, "unprioritised")
+  }
+
+  //create object with keys as priority levels and values as arrays of corresponding issues
+  var prioritised_issues = entries.reduce(function(prioritised_issues, issue) {
+
+    //selects the highest priority digit for that issue ("unprioritised" if no priority)
+    var priority = decide_priority(find_priorities(issue));
+
+    if (!prioritised_issues[priority]) {
+      prioritised_issues[priority] = [];
+    }
+
+    prioritised_issues[priority].push(issue);
+
+    return prioritised_issues;
+
+  },{});
+
+  //restructure prioritised_issues into an array of objects with priority and issues properties
+  var prioritised_array = Object.keys(prioritised_issues).map(function(priority) {
+    return {
+      priority: priority,
+      issues: prioritised_issues[priority]
+    }
+  });
+
+  return prioritised_array;
 }
--- test/issue_parser.test.js
+++ test/issue_parser.test.js
@@ -1,14 +1,14 @@
 var test  = require('tape');
 var parse = require('../handlers/issues_handler.js').parse;
-var issues = require('./fixtures/issues.json')
+var issues = require('./fixtures/issues.json');
 
-var results = parse(issues);
+var parsed_issues = parse(issues.entries);
 
 test('Parser returns array of objects', function (t) {
 
-  t.ok(Array.isArray(parse(issues)), 'returns array');
+  t.ok(Array.isArray(parsed_issues), 'returns array');
 
-  var are_objects = results.every(function (result) {
+  var are_objects = parsed_issues.every(function (result) {
     return (typeof result === 'object');
   });
   t.ok(are_objects, 'all members are objects');
@@ -22,7 +22,7 @@ test('Array of objects have both "priority" and "issues" properties', function (
   var has_issue_and_priority_properties = function (result) {
     return result.priority !== undefined && result.issues !== undefined;
   };
-  var have_properties = results.every(has_issue_and_priority_properties);
+  var have_properties = parsed_issues.every(has_issue_and_priority_properties);
 
   t.ok(have_properties, 'all objects have priority and issues properties');
 
@@ -30,37 +30,38 @@ test('Array of objects have both "priority" and "issues" properties', function (
 
 });
 
-test('"priority" and "issue" are of the expected types', function (t) {
-
-  var has_expected_types = function (result) {
-    return typeof result.priority === 'number' && Array.isArray(result.issues);
-  };
-  var have_types = results.every(has_expected_types);
-
-  t.ok(have_types, '"priority" and "issue" have expected types');
-
-  t.end();
-
-});
 
 test('parsed issues have expected structure', function(t) {
-  function is_between_1_and_5 (num) {
-    return typeof num === 'number' && num >= 1 && num <= 5;
+
+  function check_issues (priority) {
+    return Array.isArray(priority.issues);
   }
+
+  function is_valid_priority (priority) {
+    return ['1', '2', '3', '4', '5', 'unprioritised'].some(function(level){
+      return level === priority;
+    });
+  }
+
   function has_title_and_url (issue) {
     return issue.title !== undefined && issue.url !== undefined;
   }
 
-  var valid_priorities = results.every(function (result) {
-    return is_between_1_and_5(result.priority);
-  });
-  t.ok(valid_priorities, 'priorities are in expected range');
+  var issues_is_array = parsed_issues.every(check_issues);
+  t.ok(issues_is_array, '"priority" and "issue" have expected types');
 
-  var valid_issues = results.every(function(result) {
-    return result.issues.length && result.issues.every(has_title_and_url);
+  var valid_priorities = parsed_issues.every(function (priority_level) {
+    return is_valid_priority(priority_level.priority);
+  });
+  t.ok(valid_priorities, 'priorities are valid');
+
+  var valid_issues = parsed_issues.every(function(priority) {
+    return priority.issues.length && priority.issues.every(has_title_and_url);
   });
   t.ok(valid_issues, 'issues have title and url properties');
+
   t.end();
+
 });
 
 
