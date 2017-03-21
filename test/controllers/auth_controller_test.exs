aeb1c5ed8d95ec7a0fb39bad2a48832e3292ced5
jbarget <dear.justen@gmail.com>
2017-03-21 23:31:16 +0000

    removing redundant comments in auth test. #147

--- test/controllers/auth_controller_test.exs
+++ test/controllers/auth_controller_test.exs
@@ -2,13 +2,10 @@ defmodule Tudo.AuthControllerTest do
   use Tudo.ConnCase
 
   test "GET /auth/github/callback", %{conn: conn} do
-    # transform conn to have conn.assigns have %{ueberauth_auth} property
     conn = put_in(conn.assigns, %{ueberauth_auth: %{}})
     conn = get conn, "/auth/github/callback"
 
     assert get_flash(conn, :info) =~ "sucessfully authenticated"
-
-    # assert here
   end
 
   test "GET /auth/github/callback with errors", %{conn: conn} do
