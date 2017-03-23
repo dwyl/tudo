6bbc2f8cb35c8f69c67eb695750ded7ce8474056
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-03-23 07:40:47 +0000

    adds status code and html tests to auth controller
    related #147

--- test/controllers/auth_controller_test.exs
+++ test/controllers/auth_controller_test.exs
@@ -5,12 +5,14 @@ defmodule Tudo.AuthControllerTest do
     conn = put_in(conn.assigns, %{ueberauth_auth: %{}})
     conn = get conn, "/auth/github/callback"
 
+    assert html_response(conn, 302) =~ "redirected"
     assert get_flash(conn, :info) =~ "sucessfully authenticated"
   end
 
   test "GET /auth/github/callback with errors", %{conn: conn} do
     conn = get conn, "/auth/github/callback"
 
+    assert html_response(conn, 302) =~ "redirected"
     assert get_flash(conn, :info) =~ "error while authenticating"
   end
 
