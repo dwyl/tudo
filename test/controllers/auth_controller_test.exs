f731959d1e946875a66077f22921fe0658201c10
Eoin McCarthy <em1081@my.bristol.ac.uk>
2017-03-30 14:43:17 +0000

    fix tests
    related #147

--- test/controllers/auth_controller_test.exs
+++ test/controllers/auth_controller_test.exs
@@ -1,8 +1,10 @@
 defmodule Tudo.AuthControllerTest do
   use Tudo.ConnCase
 
+  alias Ueberauth.Auth
+
   test "GET /auth/github/callback", %{conn: conn} do
-    conn = put_in(conn.assigns, %{ueberauth_auth: %{}})
+    conn = put_in(conn.assigns, %{ueberauth_auth: %Auth{}})
     conn = get conn, "/auth/github/callback"
 
     assert html_response(conn, 302) =~ "redirected"
