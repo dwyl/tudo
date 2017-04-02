e288b27f92e254dc9c0d0f75f8388e6444dbfe77
Eoin McCarthy <em1081@my.bristol.ac.uk>
2017-04-02 12:52:08 +0000

    adds test for delete session
    related #147

--- test/controllers/auth_controller_test.exs
+++ test/controllers/auth_controller_test.exs
@@ -8,6 +8,7 @@ defmodule Tudo.AuthControllerTest do
     conn = get conn, "/auth/github/callback"
 
     assert html_response(conn, 302) =~ "redirected"
+    assert get_session(conn, :current_user)
     assert get_flash(conn, :info) =~ "sucessfully authenticated"
   end
 
@@ -18,4 +19,13 @@ defmodule Tudo.AuthControllerTest do
     assert get_flash(conn, :info) =~ "error while authenticating"
   end
 
+  test "DELETE /auth/logout", %{conn: conn} do
+    conn = put_in(conn.assigns, %{ueberauth_auth: %Auth{}})
+    conn = get conn, "/auth/github/callback"
+    conn = delete conn, "/auth/logout"
+
+    assert get_flash(conn, :info) =~ "logged out"
+    assert html_response(conn, 302) =~ "redirected"
+    assert conn.private[:plug_session_info] == :drop
+  end
 end
