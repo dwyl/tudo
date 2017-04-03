c9307f43f97abd0ba179b72398ab8f78b43408dd
jbarget <dear.justen@gmail.com>
2017-04-03 18:45:03 +0100

    WIP, adding logic to retrieve list of repos for given org. #148

--- web/controllers/github_controller.ex
+++ web/controllers/github_controller.ex
@@ -1,16 +1,31 @@
-defmodule Tudo.Github do
+defmodule Tudo.GithubController do
   use Tudo.Web, :controller
   use HTTPoison.Base
-  alias Tudo.Github
+
+  alias Tudo.GithubController
 
   @endpoint "https://api.github.com"
 
   def process_url(url) do
-    @endpoint <> url <> "?access_token=access_token here"
+    @endpoint <> url <> "?access_token=f39b939ad9af6a984c0cba760e91bfa8143582a3"
   end
 
-  def index(repo) do
-    Github.get("/repos/dwyl/" <> repo <> "/issues")
+  def index(conn, %{"org" => org}) do
+    {:ok, res} = GithubController.get("/orgs/#{org}/repos")
+
+    res.body
+    |> parse_json
+    |> get_issues
+
+    conn
+  end
+
+  defp get_issues(repos) do
+    # for each repo go and get the issues
+  end
+
+  defp parse_json(body) do
+    {:ok, Poison.Parser.parse!(body)}
   end
 
 end
