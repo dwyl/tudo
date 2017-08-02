75768157c21191448b08bec2a0e5e07c4e2154ed
Sam Houston <shouston3@me.com>
2017-08-02 15:22:42 +0100

    remove dev debug logging to make the server logs easier to read

--- config/dev.exs
+++ config/dev.exs
@@ -27,7 +27,7 @@ config :tudo, Tudo.Endpoint,
   ]
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, format: "[$level] $message\n", level: :info
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
