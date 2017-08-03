3c01009a20dd37caffed4094625c7930f8b1f69d
jbarget <dear.justen@gmail.com>
2017-08-03 14:04:50 +0100

    refactor for credo linting

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -24,7 +24,12 @@ defmodule Tudo.PageView do
 
   def get_label_data(issue) do
     issue.labels
-    |> Enum.map(fn label -> String.split(label, ";") |> List.to_tuple |> get_label_font_color end)
+    |> Enum.map(fn label ->
+      label
+      |> String.split(";")
+      |> List.to_tuple
+      |> get_label_font_color
+    end)
   end
 
   def get_label_font_color({label_hex, label_name}) do
@@ -32,9 +37,9 @@ defmodule Tudo.PageView do
     |> String.replace("#", "")
     |> String.to_integer(16)
 
-    cond do
-      rgb_value > 0xffffff / 2 -> {label_hex, label_name, '#000'}
-      rgb_value <= 0xffffff / 2 -> {label_hex, label_name, '#fff'}
+    case rgb_value > 0xffffff / 2 do
+      true -> {label_hex, label_name, '#000'}
+      false -> {label_hex, label_name, '#fff'}
     end
   end
 
