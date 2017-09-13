266f4cd0da869466692a496b7157dce931f18235
Zooey <zooey@lbk.org.uk>
2017-09-13 16:22:27 +0100

    beginnings of a search by labels due to the problems with rummage this does not work and may need some serious reworking. commiting to preserve work and move on to something else #238

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -9,10 +9,14 @@ defmodule Tudo.PageController do
     {issues, rummage} =
       case params do
         %{"rummage" => rummage_param, "search" => search_params} ->
+          IO.inspect(search_params)
           get_issues(rummage_param, search_params)
         _ ->
           get_issues(params["rummage"])
       end
+
+      IO.inspect(rummage)
+
 
     render conn,
       "index.html",
@@ -28,10 +32,17 @@ defmodule Tudo.PageController do
   end
 
   defp get_issues(rummage_param) do
-    {query, rummage} = Issue
+    {_query, rummage} = Issue
       |> Ecto.rummage(rummage_param)
 
+    IO.inspect(query)
+
+    query = from x in Issue,
+            where: ilike repo_name, search_params["repo_name"]
+
+
     {Repo.all(query), rummage}
+    |> construct_issues_and_rummage(["enhancement"])
   end
 
   defp get_issues(rummage_param, search_params) do
@@ -40,9 +51,72 @@ defmodule Tudo.PageController do
         "search",
         Map.merge(rummage_param["search"], %{"repo_name" => %{"assoc" => [],
                                              "search_term" => search_params["repo_name"],
-                                             "search_type" => "ilike"}})
+                                             "search_type" => "ilike"},
+                                             })
                            )
+
     |> get_issues
+  end
+
+  def construct_issues_and_rummage({issues, rummage}, labels_to_search_for) do
+    new_issues = filter_issues_by_labels(issues, labels_to_search_for)
+    new_rummage = construct_rummage(new_issues, rummage)
+
+    {new_issues, new_rummage}
+  end
+
+  def filter_issues_by_labels(list_of_issues, labels_to_search_for) do
+    # list of issues
+    # |> filter ( %{label} ->  mapped_labels = (label.map split ; [1]) mapped_labels contains_all label_options )
+    list_of_issues
+    |> Enum.filter(fn %Tudo.Issue{labels: labels} ->
+      contains_all_labels?(labels, labels_to_search_for)
+    end)
+  end
+
+  def construct_rummage(list_of_issues, rummage) do
+    # %{"paginate" => %{"max_page" => "54", "page" => "1", "per_page" => "20",
+    # "total_count" => "1065"}, "search" => %{}, "sort" => %{}}
+    total_count = length(list_of_issues)
+    max_page = get_page_count(total_count)
+
+    Map.put(
+      rummage,
+      "paginate",
+      Map.merge(rummage["paginate"], %{
+          "total_count" => total_count |> Integer.to_string,
+          "max_page" => max_page |> Integer.to_string
+          })
+    )
+
+
+  end
+
+  defp get_page_count(number_of_issues) do
+    innacurate_page_number = div(number_of_issues, 20)
+    case rem(number_of_issues, 20) do
+      0 ->
+        innacurate_page_number
+      _ ->
+        innacurate_page_number + 1
+    end
+
+  end
+
+  defp contains_all_labels?(label_list, labels_to_search_for) do
+    only_label_name_list =
+      label_list
+      |> Enum.map(fn label ->
+        [_head | label_name] = String.split(label, ";")
+        label_name
+      end)
+      |> List.flatten
+
+    IO.inspect(only_label_name_list)
+    Enum.all?(labels_to_search_for, fn label_name ->
+      Enum.member?(only_label_name_list, label_name)
+    end)
+
   end
 
   defp get_repos do
