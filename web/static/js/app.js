240e12de9b53aa5a441316270955b9350a49d1b8
Zooey <zooey@lbk.org.uk>
2017-09-14 11:49:40 +0100

    adds accessible-autocomplete to layout #234

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,7 +11,8 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import 'phoenix_html';
+import accessibleAutocomplete from 'accessible-autocomplete';
 
 // Import local files
 //
@@ -19,3 +20,13 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+//
+// const repos = ['learn-elixir', 'time', 'tudo'];
+
+accessibleAutocomplete({
+  element: document.querySelector('#my-autocomplete-container'),
+  id: 'my-autocomplete', // To match it to the existing <label>.
+  source: repos,
+  name: 'issue[repo_name]',
+  displayMenu: 'overlay',
+});
